b152d1a3a349151a493ce2d63fbf3d06bd5b5a21
Matheus <matheusestrelab@hotmail.com>
2021-12-14 21:56:59 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿// See https://aka.ms/new-console-template for more information
+class TextEditor{
+    static void Main(string []args){
+        Menu();
+    }
+
+    static void Menu(){
+        Console.Clear();
+        Console.WriteLine("O que você deseja fazer? ");
+        Console.WriteLine("1- Abrir arquivo");
+        Console.WriteLine("2- Criar um arquivo");
+        Console.WriteLine("0- Sair");
+        short option = short.Parse(Console.ReadLine());
+
+        Console.Clear();
+
+        switch(option){
+            case 0 : System.Environment.Exit(0);
+            break;
+
+            case 1 : Abrir();
+            break;
+
+            case 2 : Editar();
+            break;
+
+            default : Menu();
+            break;
+        }
+    }
+
+    static void Abrir(){
+        Console.Clear();
+        Console.WriteLine("Qual caminho do arquivo que desejas abrir ?");
+        string path = Console.ReadLine();
+
+        using (var file = new StreamReader(path))
+        {
+            string text = file.ReadToEnd();
+            Console.WriteLine(text);
+        }
+
+        Console.WriteLine("");
+        Console.ReadLine();
+        Menu();
+
+
+    }
+
+    static void Editar(){
+        Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+        Console.WriteLine("---------------------------------------");
+        string text = "";
+
+        do
+        {
+            text += Console.ReadLine();
+            text += Environment.NewLine;
+        }
+        while (Console.ReadKey().Key != ConsoleKey.Escape);
+        Salvar(text);
+    }
+
+    static void Salvar(string text){
+        Console.Clear();
+        Console.WriteLine("Qual o caminho para salvar o arquivo ?");
+        var path = Console.ReadLine();
+
+        using(var file = new StreamWriter(path))
+        {
+            file.Write(text);
+        }
+
+        Console.WriteLine($"Arquivo salvo com sucesso no caminho {path}");
+        Console.ReadLine();
+        Menu();
+
+    }
+
+}
+
